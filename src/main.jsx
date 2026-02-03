627926413cfc81bae2be3147546273363fff1072
anka reza <rezawalker02@gmail.com>
2026-02-03 18:00:45 +0700

    Refactor App component: move from App.jsx to App.js and update import path in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import './output.css'
-import App from './App.jsx'
+import App from './App.js'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
